f3597b23e8131f4092692ed03de9166efab6e599
Carlos Orue <carlos@orue.io>
2025-01-12 19:55:39 -0600

    Adding template and data structure

--- main.go
+++ main.go
@@ -2,16 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	"orue.io/go/museum/data"
 )
 
 func handleHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from a Go server"))
 }
 
+func handleTemplate(w http.ResponseWriter, r *http.Request) {
+	html, err := template.ParseFiles("templates/index.tmpl")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+	html.Execute(w, data.GetAll()[0])
+}
+
 func main() {
 	server := http.NewServeMux()
 	server.HandleFunc("/hello", handleHello)
+	server.HandleFunc("/templates", handleTemplate)
 
 	fs := http.FileServer(http.Dir("./public"))
 	server.Handle("/", fs)
